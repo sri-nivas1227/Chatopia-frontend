235f9bedbadeba3acb17ebdfa375ead3e4d034b7
Sri-1227 <srinivasmekala1227@gmail.com>
2023-06-25 23:04:26 +0530

    tailwind project init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        Amatic: ["Amatic SC", "cursive"],
+        Kalam: ["Kalam", "cursive"],
+        Moirai: ["Moirai One", "cursive"],
+        PS2P: ["Press Start 2P", "monospace"],
+        Rubik: ["Rubik Moonrocks", "cursive"],
+      },
+    },
+  },
+  plugins: [],
+};
